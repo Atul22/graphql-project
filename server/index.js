8ecc02bfd65454fada7b2ed779399964a22e78d0
Atul22 <atuliitrpr@gmail.com>
2019-05-12 23:31:12 +0530

    Add mongo support using mLab

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const mongoose = require("mongoose");
 const schema = require("./schema/schema");
 
 const app = express();
+
+mongoose.connect(
+	'mongodb+srv://oggy:test123@cluster0-xligh.mongodb.net/test?retryWrites=true',
+	{ useNewUrlParser: true }
+);
+mongoose.connection.once('open', () => {
+	console.log('Connected to DB');
+})
 
 app.use('/graphql', graphqlHTTP({
 	schema,
